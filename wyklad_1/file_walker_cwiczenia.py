7f81427cb9b3027fe7d458cf4e35bae1f9da04a0
Paweł Kordowski <paw.mar.kor@gmail.com>
2017-05-20 18:01:21 +0200

    L1T3: Compute size of main dir + Exception handling

--- wyklad_1/file_walker_cwiczenia.py
+++ wyklad_1/file_walker_cwiczenia.py
@@ -1,53 +1,61 @@
+import logging
 import os
+from pprint import pprint
 
-def sample_directory_walker(new_cwd=None):
-    old_cwd = os.getcwd()
-    if new_cwd:
-        os.chdir(new_cwd)
-    # to nam nie daje wszystkich wyników (pomija foldery)
-    for root, dirs, files in os.walk(os.getcwd(), topdown=False):
-        print('{}, {}, {}\n'.format(root, dirs, files))
-    if new_cwd:
-        os.chdir(old_cwd)
-        
-# sample_directory_walker('/home/marcin/kursy')
-def format_sizes(sizes):
-    keys = list(sizes.keys())
-    keys.sort()
-    entry_format = '\t{}: {}'
-    entry_lines = (entry_format.format(k, sizes[k]) for k in keys)
-    return '{}\n{}\n{}'.format('{', '\n'.join(entry_lines), '}')
+logger = logging.getLogger(__name__)
 
 
-def my_directory_walker_with_size_counting(topdir=None):
+class EntrySizeComputingError(Exception):
+    def __init__(self, path, errors):
+        self.errors = errors
+        self.path = path
+
+    def __str__(self):
+        return "Unable to compute size of {} because of ({})".format(
+            self.path,
+            ",".join(str(error) for error in self.errors)
+        )
+
+
+def my_dir_walker_with_size_counting(topdir=None):
     if topdir is None:
         topdir = os.getcwd()
-    sizes = {topdir: 0}
-    root_stack = []
-    current_root_size = 0
-    
-    def inner_walker(new_topdir):
-        root_stack.append(new_topdir)
-        new_topdir_path = os.path.join(*root_stack)
-        # TODO: PRACA DOMOWA: dodać obsługę błędów
-        entries = os.scandir(new_topdir_path)
+    size_of_file = {}
+    dir_tree_stack = []
+
+    def inner_walker(curr_dir):
+        current_path = os.path.join(*dir_tree_stack, curr_dir)
+        entries = os.scandir(current_path)
+        dir_tree_stack.append(curr_dir)
         size = 0
+        errors = []
         for entry in entries:
-            if entry.is_dir(follow_symlinks=False):
-                entry_size = inner_walker(entry.name)
-                sizes[os.path.join(*root_stack)] = entry_size
-                size += entry_size
-                root_stack.pop()
-            elif entry.is_file(follow_symlinks=False):
-                sizes[os.path.join(*root_stack, entry.name)] = entry.stat().st_size
-                size += entry.stat().st_size # os.path.getsize
+            if entry.is_symlink():
+                continue
+            file_path = os.path.join(*dir_tree_stack, entry.name)
+            try:
+                if entry.is_dir():
+                    current_size = inner_walker(entry.name)
+                elif entry.is_file():
+                    current_size = entry.stat().st_size
+            except (OSError, EntrySizeComputingError) as an_error:
+                logger.error("Unable to compute size for {} because of ({})"
+                             .format(file_path, an_error))
+                errors.append(an_error)
+            else:
+                size_of_file[file_path] = current_size
+                size += current_size
+        dir_tree_stack.pop()
+        if errors:
+            raise EntrySizeComputingError(current_path, errors)
         return size
-    inner_walker(topdir)
-    return sizes
-    
 
-sizes = my_directory_walker_with_size_counting('/home/marcin/kursy')
-#sizes = my_directory_walker_with_size_counting('/home/marcin')
+    try:
+        size_of_file[topdir] = inner_walker(topdir)
+    except (OSError, EntrySizeComputingError) as error:
+        logger.error("Unable to compute size for {} because of ({})"
+                     .format(topdir, error))
+    return size_of_file
 
-print(format_sizes(sizes))
-print(len(sizes))
+if __name__ == "__main__":
+    pprint(my_dir_walker_with_size_counting("/home/pawkor/daftcode"))
