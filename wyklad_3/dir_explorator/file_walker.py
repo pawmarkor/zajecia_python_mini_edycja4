a1276ae19c8a430a77a209dce6005901faa38cbb
Paweł Kordowski <paw.mar.kor@gmail.com>
2017-05-21 09:22:36 +0200

    L3: Entity_type as Enum + encapsulation (is_dir/file)

--- wyklad_3/dir_explorator/file_walker.py
+++ wyklad_3/dir_explorator/file_walker.py
@@ -1,9 +1,22 @@
 import logging
+from enum import Enum
 from pathlib import Path
 from pprint import pprint
 from collections import namedtuple
 
-EntityData = namedtuple('EntityData', 'type size children')
+
+class EntityType(Enum):
+    FILE = 'FILE'
+    DIR = 'DIR'
+
+
+class EntityData(namedtuple('EntityData', 'type size children')):
+    def is_dir(self):
+        return self.type == EntityType.DIR
+
+    def is_file(self):
+        return self.type == EntityType.FILE
+
 
 logger = logging.getLogger(__name__)
 
@@ -27,10 +40,10 @@ def my_dir_walker_with_size_counting(topdir=None, topname=None):
             if entry.is_symlink():
                 continue
             if entry.is_dir():
-                entry_type = 'dir'
+                entry_type = EntityType.DIR
                 entry_size, entry_children = inner_walker(entry.name)
             elif entry.is_file():
-                entry_type = 'file'
+                entry_type = EntityType.FILE
                 entry_size = entry.stat().st_size
                 entry_children = []
             children[entry.name] = EntityData(
